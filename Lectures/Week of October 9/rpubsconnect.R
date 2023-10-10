9e543f55a12c846254ecbe87d47143d32c0b22c5
Tom Hanna <tlhanna@uh.edu>
2023-10-09 22:02:15 -0500

    October 9 and test of rpubs

---
+++ Lectures/Week of October 9/rpubsconnect.R
@@ -0,0 +1,4 @@
+rsconnect::rpubsUpload("October9_Lectures","~/R_Projects/UHGOVT2306/Lectures/Week of October 9/week of october 9.html","~/R_Projects/UHGOVT2306/Lectures/Week of October 9/week of october 9.qmd", id = "https://api.rpubs.com/api/v1/document/1096172/73749cecee8f4ca48ee4cd006aa2d204")
+
+
+
